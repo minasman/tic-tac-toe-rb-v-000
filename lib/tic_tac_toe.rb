3861bd0222b6a80f42a8a5efd0a6fb24b27aabb1
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-13 03:05:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,7 @@ end
 def valid_move?(board, index)
   if index < 0 || index > 8
     return FALSE
-  elsif position_taken(board, index)
+  elsif position_taken?(board, index)
     return FALSE
   else
     return TRUE
